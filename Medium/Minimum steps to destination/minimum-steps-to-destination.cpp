b6e391159a37f7251533d7de4c9866de81b192bd
swamyyalla <97425775+Yallaswamy@users.noreply.github.com>
2024-05-12 17:43:25 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Minimum steps to destination/minimum-steps-to-destination.cpp
@@ -0,0 +1,42 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    int minSteps(int d) {
+        // code here
+        int sum=0,s=0;
+        while(sum<d){
+            sum+=s;
+            s++;
+        }
+        while((sum-d)%2){
+            sum+=s;
+            s+=1;
+        }
+        return s-1;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int d;
+        cin >> d;
+
+        Solution ob;
+        cout << ob.minSteps(d) << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
